605074dda0db0a02ad7f6938f02ec353ce149c90
Saumya <64534496+27Saumya@users.noreply.github.com>
2024-09-21 02:35:53 +0530

    Add tmux config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 starship init fish | source
+fzf --fish | source
 thefuck --alias | source
 
 set PATH $PATH ~/.cargo/bin
