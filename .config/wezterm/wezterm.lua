c1ff1a52d4a3cef5427cdc1a23f744a348a12373
Saumya <64534496+27Saumya@users.noreply.github.com>
2024-09-26 11:56:49 +0530

    Pre-define wezterm window size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,12 +5,16 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("Fira Code")
 config.font_size = 15.0
 
-config.color_scheme = "Dracula"
+config.color_scheme = "Catppuccin Macchiato"
 
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 24
+config.initial_cols = 92
+config.initial_rows = 32
+
+config.max_fps = 120
 
 return config
